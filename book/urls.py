0f9ae1a271b4f289deab6785b58104820868e006
slpark8888 <slpark8888@gmail.com>
2022-03-14 09:11:28 +0900

    pyweb

--- book/urls.py
+++ book/urls.py
@@ -5,4 +5,5 @@ app_name = 'book'  # 네임 스페이스(소속)
 
 urlpatterns = [
     path('book/', views.django_doc, name='django_doc'),  # 127.0.0.1/poll/
+    path('about_me/', views.about_me, name='about_me'),  # 127.0.0.1/poll/
 ]
--- book/views.py
+++ book/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def django_doc(request):
     return render(request, 'book/django_set_doc.html')
+
+
+def about_me(request):
+    return render(request, 'book/about_me.html')
